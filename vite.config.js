f2eb9c54da627e5ea5aa3846833e10721ed96dd9
Drewcrich <drewcrichardson1999@gmail.com>
2025-07-09 14:57:41 -0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  preview: {
+    host: '0.0.0.0',
+    port: 3000,
+    strictPort: true
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000
+  }
 })
